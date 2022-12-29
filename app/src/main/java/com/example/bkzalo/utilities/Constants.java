134dc7452d3383171c700310ae431307b770f859
Tobi <dinhnguyen2002asd@gmail.com>
2022-12-29 15:33:09 +0700

    bản full

--- app/src/main/java/com/example/bkzalo/utilities/Constants.java
+++ app/src/main/java/com/example/bkzalo/utilities/Constants.java
@@ -14,5 +14,5 @@ public class Constants {
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USERMODEL="usermodel";
     public static final String KEY_GROUP="group";
-    public static final String KEY_API = "http://192.168.1.2:8080/" ;
+    public static final String KEY_API = "http://192.168.2.203:8080/" ;
 }
